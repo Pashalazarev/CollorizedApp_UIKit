ca8b44672bb668f623b6fac9ede93f27cdfcd051
Pashalazarev <mymessagebox9@gmail.com>
2022-11-09 13:10:14 +0300

    Initial Commit

---
+++ CollorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollorizedApp
+//
+//  Created by Pavel Lazarev Macbook on 09.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
